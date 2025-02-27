2b4695808bfea7c8359d485a73c03c23729693e5
igorpaes21 <irsp2121@hmail.com>
2025-02-26 22:06:25 -0300

    3° semana, array list, capturar elementos, remover elementos, verificar valores de uma lista

---
+++ src/ArrayList/Main.java
@@ -0,0 +1,49 @@
+package ArrayList;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        ArrayList<Integer> numeros = new ArrayList<>();
+
+        System.out.println(numeros.size());
+
+        //Adicionando valores na lista
+        numeros.add(1);
+        numeros.add(2);
+        numeros.add(3);
+        numeros.add(4);
+
+        System.out.println(numeros.size());
+
+        //Capturar um elemento
+
+        System.out.print("Qual posição deseja capturar: ");
+        int posicao = scan.nextInt();
+
+        System.out.printf("Qual posição %d %d", posicao, numeros.get(posicao-1));
+
+        //Remover um elemento
+        System.out.print("\nQual posição deseja remover: ");
+        posicao = scan.nextInt();
+
+        System.out.printf("Valor %d posição %d foi removido", numeros.get(posicao-1), posicao);
+        numeros.remove(posicao-1);
+
+        //Verificar valores contidos na lista
+
+        System.out.print("Valor para ser procurado: ");
+        int valor = scan.nextInt();
+
+        System.out.println(numeros.contains(valor) ? "Valor contido na lista" : "Valor não encontrado!");
+
+        for (Integer numero : numeros){
+            System.out.print(numero + "\t");
+
+        }
+
+    }
+}
--- src/matrizes/Main.java
+++ src/matrizes/Main.java
@@ -4,12 +4,31 @@ public class Main {
     public static void main(String[] args) {
         int [][] valores = new int[2][3];
 
+        //Declarar e Inicializar uma matriz
+        String[][] matriz = {
+                {"A", "B", "C"},
+                {"D", "E","F"},
+                {"G","H","I"}
+        };
+        String[][] letras = matriz;
+
+
         System.out.println("Quantidade de linhas:" + valores.length);
         System.out.println("Quantidade de colunas: " + valores[0].length);
 
         valores [1][1] = 10;
 
         System.out.println(valores[1][1]);
+
+        //iterar a nossa matriz
+
+        for (int linha = 0; linha < matriz.length; linha++){
+            for (int coluna = 0; coluna < matriz[0].length; coluna++){
+                System.out.println(matriz[linha][coluna] + "\t");
+            }
+            System.out.println(matriz);
+            System.out.println(linha);
+        }
 
 
     }
